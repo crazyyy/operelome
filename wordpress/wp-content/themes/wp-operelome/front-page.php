f4744191bed50ecfd8febe030de5a7244f9a174b
crazyyy <crazyyy@gmail.com>
2015-12-02 21:19:37 +0200

    frontpage ready

--- wordpress/wp-content/themes/wp-operelome/front-page.php
+++ wordpress/wp-content/themes/wp-operelome/front-page.php
@@ -14,7 +14,7 @@
                     <input class="search-text" type="text" name="s" value="" placeholder="Введите запрос..." required="required">
                   </div>
                 </form>
-                <a href="http://operelome.ru/zadat-vopros" class="bordered-button bordered-button-doctor">
+                <a href="<?php echo home_url(); ?>/zadat-vopros-vrachu.htm" class="bordered-button bordered-button-doctor">
                   <span><strong>Задать вопрос<br> травматологу</strong></span>
                 </a>
                 <a href="http://operelome.ru/poisk-travmpunkta" class="bordered-button bordered-button-map">
@@ -34,17 +34,18 @@
                       </div>
                       <div class="col col-16">
                         <ul class="questions-list">
-                          <li><a href="http://operelome.ru/vopros/posle-pereloma-lonnoy-kosti-nachalis-silnyie-boli">После перелома лонной кости начались сильные боли?</a></li>
-                          <li><a href="http://operelome.ru/vopros/posle-seansa-magnitoterapii-nachinayutsya-rezkie-boli-v-oblasti-pereloma-klyuchitsyi">После сеанса магнитотерапии начинаются резкие боли в области перелома ключицы</a></li>
-                          <li><a href="http://operelome.ru/vopros/perelom-pozvonka-1-y-stepeni-mozhno-li-vstavat">Перелом позвонка 1-й степени — можно ли вставать?</a></li>
-                          <li><a href="http://operelome.ru/vopros/iz-za-skolioza-noshu-korset-mozhno-li-v-nem-katatsya-na-skeyte">Из-за сколиоза ношу корсет, можно ли в нем кататься на скейте?</a></li>
+                        <?php query_posts( array( 'post_type' => faq, 'showposts' => 4 ) ); ?>
+                          <?php while ( have_posts() ) : the_post(); ?>
+                            <li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
+                          <?php endwhile; ?>
+                        <?php wp_reset_query(); ?>
                         </ul>
                       </div>
                       <div class="clear"></div>
                     </div>
                     <div class="text-right" style="margin-top: 20px;">
-                      <a class="btn-filled btn-filled-small" href="http://operelome.ru/zadat-vopros" title="">Задать свой вопрос</a>
-                      <a class="btn-filled btn-filled-small" href="http://operelome.ru/vopros" title="">Все вопросы</a>
+                      <a class="btn-filled btn-filled-small" href="<?php echo home_url(); ?>/zadat-vopros-vrachu.htm">Задать свой вопрос</a>
+                      <a class="btn-filled btn-filled-small" href="<?php echo home_url(); ?>/faq" title="">Все вопросы</a>
                     </div>
                   </div>
                 </div>
@@ -55,70 +56,79 @@
               <div class="clear"></div>
             </div>
             <div class="row row-posts">
-              <div class="col col-12">
+
+              <?php
+                $firstCatId = '9';
+                $catTitle = get_cat_name( $firstCatId );
+                $catLink = get_category_link( $firstCatId );
+              ?>
+
+              <div class="col col-12 first-block">
                 <div class="block-title">
-                  Первая помощь при переломе
-                  <a class="more" href="http://operelome.ru/pervaya-pomosch" title="Первая помощь при переломе"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-link.png" alt=""></a>
-                </div>
+                  <?php echo $catTitle ?>
+                  <a class="more" href="<?php echo $catLink ?>" title="<?php echo $catTitle ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-link.png" alt=""></a>
+                </div><!-- block-title -->
+
                 <div class="short-horiz-wrapper">
-                  <div class="short-horiz" id="short-horiz-1509">
+
+                  <?php query_posts("showposts=3&cat=' . $firstCatId . '"); ?>
+                  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                  <div class="short-horiz" id="short-horiz-<?php the_ID(); ?>">
                     <div class="img">
                       <div class="thumb thumb-small">
-                        <a href="http://operelome.ru/pervaya-pomosch/1509-pravila-nalozheniya-shin-pri-perelomah" title="Правила наложения шин при переломах"><img src="<?php echo get_template_directory_uri(); ?>/img/ruka-na-perevyazi-mddz6kyj89s50fljma8py4ndguoarolrljk8qlu63g.jpg" title="Правила наложения шин при переломах" alt="Правила наложения шин при переломах" width="150" height="150"></a>
+                        <a href="<?php the_permalink(); ?>" title="Вдавленный перелом черепа">
+                          <?php if ( has_post_thumbnail()) :
+                            the_post_thumbnail('large');
+                          else: ?>
+                            <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+                          <?php endif; ?>
+                        </a>
                       </div>
                     </div>
-                    <div class="title"><a href="http://operelome.ru/pervaya-pomosch/1509-pravila-nalozheniya-shin-pri-perelomah" title="Правила наложения шин при переломах">Правила наложения шин при переломах</a></div>
-                  </div>
-                  <div class="short-horiz" id="short-horiz-1490">
-                    <div class="img">
-                      <div class="thumb thumb-small">
-                        <a href="http://operelome.ru/pervaya-pomosch/1490-pervaya-pomoshh-pri-perelome-nogi" title="Первая помощь при переломе ноги"><img src="<?php echo get_template_directory_uri(); ?>/img/sportsmen-s-nogoy-v-gipse-mdbnuuoom7dlg4vfxmuon3m9svym1jvv69c10tkfa4.jpg" title="Первая помощь при переломе ноги" alt="Первая помощь при переломе ноги" width="150" height="150"></a>
-                      </div>
-                    </div>
-                    <div class="title"><a href="http://operelome.ru/pervaya-pomosch/1490-pervaya-pomoshh-pri-perelome-nogi" title="Первая помощь при переломе ноги">Первая помощь при переломе ноги</a></div>
-                  </div>
-                  <div class="short-horiz" id="short-horiz-1460">
-                    <div class="img">
-                      <div class="thumb thumb-small">
-                        <a href="http://operelome.ru/pervaya-pomosch/1460-pervaya-pomoshh-pri-perelome-reber" title="Первая помощь при переломе ребер"><img src="<?php echo get_template_directory_uri(); ?>/img/sportsmen-poluchil-udar-po-rebram-md6rk8ev8vc2c7ovq9v14nbc5tmxpo8ylevoq7rfq4.jpg" title="Первая помощь при переломе ребер" alt="Первая помощь при переломе ребер" width="150" height="150"></a>
-                      </div>
-                    </div>
-                    <div class="title"><a href="http://operelome.ru/pervaya-pomosch/1460-pervaya-pomoshh-pri-perelome-reber" title="Первая помощь при переломе ребер">Первая помощь при переломе ребер</a></div>
-                  </div>
-                </div>
-              </div>
-              <div class="col col-12 last">
+                    <div class="title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></div>
+                  </div><!-- short-horiz -->
+                  <?php endwhile; endif; ?>
+                  <?php wp_reset_query(); ?>
+                </div><!-- short-horiz-wrapper -->
+              </div><!-- first-block -->
+
+              <?php
+                $secondCat = '8';
+                $catTitle = get_cat_name( $secondCat );
+                $catLink = get_category_link( $secondCat );
+              ?>
+
+              <div class="col col-12 last last-block">
                 <div class="block-title">
-                  Реабилитация после переломов
-                  <a class="more" href="http://operelome.ru/reabilitatsiya" title="Реабилитация после переломов"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-link.png" alt=""></a>
-                </div>
+                  <?php echo $catTitle ?>
+                  <a class="more" href="<?php echo $catLink ?>" title="<?php echo $catTitle ?>"><img src="<?php echo get_template_directory_uri(); ?>/img/icon-link.png" alt=""></a>
+                </div><!-- block-title -->
+
                 <div class="short-horiz-wrapper">
-                  <div class="short-horiz" id="short-horiz-1520">
+
+                  <?php query_posts("showposts=3&cat=' . $secondCat . '"); ?>
+                  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+                  <div class="short-horiz" id="short-horiz-<?php the_ID(); ?>">
                     <div class="img">
                       <div class="thumb thumb-small">
-                        <a href="http://operelome.ru/golova/1520-vdavlennyiy-perelom-cherepa" title="Вдавленный перелом черепа"><img src="<?php echo get_template_directory_uri(); ?>/img/rentegnsnimok-cherepa-mdfg2sc4foht1xmuedya6ly4jt3a9i5zehdmgigcws.jpg" title="Вдавленный перелом черепа" alt="Вдавленный перелом черепа" width="150" height="150"></a>
+                        <a href="<?php the_permalink(); ?>" title="Вдавленный перелом черепа">
+                          <?php if ( has_post_thumbnail()) :
+                            the_post_thumbnail('large');
+                          else: ?>
+                            <img src="<?php echo catchFirstImage(); ?>" title="<?php the_title(); ?>" alt="<?php the_title(); ?>" />
+                          <?php endif; ?>
+                        </a>
                       </div>
                     </div>
-                    <div class="title"><a href="http://operelome.ru/golova/1520-vdavlennyiy-perelom-cherepa" title="Вдавленный перелом черепа">Вдавленный перелом черепа</a></div>
-                  </div>
-                  <div class="short-horiz" id="short-horiz-1402">
-                    <div class="img">
-                      <div class="thumb thumb-small">
-                        <a href="http://operelome.ru/golova/1402-perelom-nosa-u-rebenka" title="Перелом носа у ребенка"><img src="<?php echo get_template_directory_uri(); ?>/img/malchik-prizhimaet-k-nosu-platok-mcxxkytjs8w1wbhfq4oj9bpsavr8a7e7epdld8qkpo.jpg" title="Перелом носа у ребенка" alt="Перелом носа у ребенка" width="150" height="150"></a>
-                      </div>
-                    </div>
-                    <div class="title"><a href="http://operelome.ru/golova/1402-perelom-nosa-u-rebenka" title="Перелом носа у ребенка">Перелом носа у ребенка</a></div>
-                  </div>
-                  <div class="short-horiz" id="short-horiz-623">
-                    <div class="img">
-                      <div class="thumb thumb-small">
-                        <a href="http://operelome.ru/golova/623-perelom-cherepa" title="Перелом черепа"><img src="<?php echo get_template_directory_uri(); ?>/img/perelom-cherepa-m8pvf14o1j068uk7qkdlw8jktcdwmneqt31p4uao9o.jpeg" title="Перелом черепа" alt="Перелом черепа" width="150" height="150"></a>
-                      </div>
-                    </div>
-                    <div class="title"><a href="http://operelome.ru/golova/623-perelom-cherepa" title="Перелом черепа">Перелом черепа</a></div>
-                  </div>
-                </div>
-              </div>
+                    <div class="title"><a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></div>
+                  </div><!-- short-horiz -->
+                  <?php endwhile; endif; ?>
+                  <?php wp_reset_query(); ?>
+
+                </div><!-- short-horiz-wrapper -->
+              </div><!-- last-block -->
+
+
               <div class="clear"></div>
             </div>
             <div class="page-wrapper" id="page-2">
--- wordpress/wp-content/themes/wp-operelome/homeslider.php
+++ wordpress/wp-content/themes/wp-operelome/homeslider.php
@@ -1,65 +1,50 @@
+<?php if( have_rows('slider') ): ?>
 <div id="home-slider-outer">
   <div id="home-slider-title">
     <div id="home-slider">
 
       <div class="sp-slides">
 
+        <?php while( have_rows('slider') ): the_row();
+
+          // vars
+          $image = get_sub_field('image');
+          $content = get_sub_field('title');
+          $link = get_sub_field('link');
+
+          ?>
+
         <div class="sp-slide">
-          <a href="http://operelome.ru/preparatyi/702-preparatyi-kaltsiya-pri-perelomah" title="Препараты кальция при переломах">
-            <img class="sp-image" src="http://operelome.ru/wp-content/uploads/thumbs/moloko-dlya-zubov-i-kostey-m8yvewn5uik3zayrr6nnh9dj4mqx777xop9nztp8xc.jpg">
-            <div class="sp-caption">Препараты кальция при переломах</div>
+          <a href="<?php echo $link; ?>" title="<?php echo $content; ?>">
+            <img class="sp-image" src="<?php echo $image; ?>" alt="<?php echo $content; ?>">
+            <div class="sp-caption"><?php echo $content; ?></div>
           </a>
         </div>
 
+        <?php endwhile; ?>
 
-        <div class="sp-slide">
-          <a href="http://operelome.ru/preparatyi/684-preparatyi-dlya-srashhivaniya-kostey" title="Препараты для сращивания костей">
-            <img class="sp-image" src="http://operelome.ru/wp-content/uploads/thumbs/mikroelementyi-v-kapsulah-m8xuge3fux6lxzm3128v20eo7sormz60r9gutmgwhc.jpg">
-            <div class="sp-caption">Препараты для сращивания костей</div>
-          </a>
-        </div>
+      </div><!-- sp-slides -->
 
-
-        <div class="sp-slide">
-          <a href="http://operelome.ru/preparatyi/669-obezbolivayushhie-tabletki-pri-perelomah" title="Обезболивающие таблетки при переломах">
-            <img class="sp-image" src="http://operelome.ru/wp-content/uploads/thumbs/mnogo-tabletok-m8xph4bkefrvnc0jukkei8ztk8all5gzfy91rs9z74.jpg">
-            <div class="sp-caption">Обезболивающие таблетки при переломах</div>
-          </a>
-        </div>
-
-
-        <div class="sp-slide">
-          <a href="http://operelome.ru/preparatyi/183-obezbolivanie-pri-perelomah" title="Обезболивание при переломах">
-            <img class="sp-image" src="http://operelome.ru/wp-content/uploads/thumbs/analgetiki-m72yr9qepyqlwayujee9voihk16oh305rcfbmji1dc.jpg">
-            <div class="sp-caption">Обезболивание при переломах</div>
-          </a>
-        </div>
-
-            </div>
 
       <div class="sp-thumbnails">
 
+          <?php while( have_rows('slider') ): the_row();
+
+            // vars
+            $image = get_sub_field('image');
+            $content = get_sub_field('title');
+            $link = get_sub_field('link');
+
+            ?>
         <div class="sp-thumbnail">
-          <div class="title">Препараты кальция при переломах</div>
+          <div class="title"><?php echo $content; ?></div>
         </div>
 
+        <?php endwhile; ?>
 
-        <div class="sp-thumbnail">
-          <div class="title">Препараты для сращивания костей</div>
-        </div>
+      </div>
 
-
-        <div class="sp-thumbnail">
-          <div class="title">Обезболивающие таблетки при переломах</div>
-        </div>
-
-
-        <div class="sp-thumbnail">
-          <div class="title">Обезболивание при переломах</div>
-        </div>
-
-            </div>
-
-    </div>
-  </div>
-</div>
+    </div><!-- home-slider -->
+  </div><!-- home-slider-title -->
+</div><!-- home-slider-outer -->
+<?php endif; ?>
